06057a321be40874bb740278bf40f579b0fb4b46
DarK7663 <mamutov9876@gmail.com>
2026-04-13 23:11:43 +0300

    env added

--- db.go
+++ db.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -8,14 +13,25 @@ import (
 var db *gorm.DB
 
 func Initialize() {
-	db, err := gorm.Open(postgres.New(postgres.Config{
-		DSN:                  "user=noonbyte password=noonbyte dbname=todo host=127.0.0.1 port=5432 sslmode=disable",
-		PreferSimpleProtocol: true,
-	}), &gorm.Config{})
-
-	if err != nil {
-		panic(err)
+	if err := godotenv.Load(); err != nil {
+		log.Println("Warning: .env file not found, using system env vars")
 	}
 
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	sslMode := os.Getenv("DB_SSLMODE")
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
+		dbHost, dbUser, dbPassword, dbName, dbPort, sslMode)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+
+	log.Println("Successfully connected to database!")
 	db.AutoMigrate(&Task{})
 }
